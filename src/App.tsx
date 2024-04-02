a8d54b8a95987ebb898f10bb2aed99df122c2d44
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2024-04-02 14:56:39 +0300

    styled-components calisti

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-
-
+import styles from  "./App.module.css"
+import Test from "./components/Test"
+import {Title} from "./components/StyledComponentTest"
 function App() {
 
     console.log(process.env.REACT_APP_API_URL)
-
+console.log(styles)
   return (
-      <div>
+      <div className={styles.App}>
 
           <h1>Hello World</h1>
-
+<Title theme="dark">Styled Components Test</Title>
           <p>{process.env.REACT_APP_API_URL}</p>
+
+          <Test/>
 
 
       </div>
---
+++ src/components/StyledComponentTest.js
@@ -0,0 +1,9 @@
+import styled, {css} from "styled-components"
+
+export const Title = styled.h1 `
+font-size:3rem;
+font-weight:700;
+${props => props.theme == "dark" && css `
+background: green;
+`}
+`
