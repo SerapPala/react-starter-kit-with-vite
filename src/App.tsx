16cf09aabbec3d1fc2d75964667181b575039717
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2024-04-02 16:37:27 +0300

    tab eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,85 @@
-import StyleComponents from "./components/styleComponents/StyleComponents";
-function App() {
+import { createElement, useState } from "react"
+import './tailwind.css'
+import Button from "./components/Button"
+import Tab from "./components/Tab"
 
-  return (
-      <div className="">
-       <StyleComponents/>
-
-      </div>
-  )
+function Profile() {
+    console.log('profil tabına geldin!')
+    return (
+        <div>
+            burası profile tabı
+        </div>
+    )
 }
 
-export default App
+function App() {
+
+    const name = 'Serap'
+    const todos = ['todo1', 'todo2', 'todo3']
+
+    const [activeTab, setActiveTab] = useState(1)
+
+    /*
+  const h1 = createElement('h1', null, 'serap.com')
+  const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
+    const button = createElement(Button, {
+        text: 'Buton Texti'
+    }, null)
+  return createElement('main', {
+    className: 'test',
+    id: 'main'
+  }, h1, ul, button)
+     */
+
+    const searchFunction = () => {
+        alert('hello!')
+    }
+
+    return (
+        <>
+
+            <div style={{padding: 20}}>
+                <button onClick={() => setActiveTab(2)}>
+                    Aktif Tabı Değiştir
+                </button>
+                <Tab activeTab={activeTab} onChange={tabIndex => setActiveTab(tabIndex)}>
+                    <Tab.Panel title="Profil"><Profile /></Tab.Panel>
+                    <Tab.Panel title="Hakkında">2. tab</Tab.Panel>
+                    <Tab.Panel title="Ayarlar">3. tab</Tab.Panel>
+                </Tab>
+                {activeTab === 2 && (
+                    <div>
+                        burası da eksta bir alan!
+                    </div>
+                )}
+            </div>
+
+            <div style={{padding: 20}}>
+                <Button>
+                    Buton Örneği
+                </Button>
+                <Button variant="success">
+                    Buton Örneği
+                </Button>
+                <Button variant="danger">
+                    Buton Örneği
+                </Button>
+                <Button variant="warning">
+                    Buton Örneği
+                </Button>
+            </div>
+            <h1 tabIndex="3" style={{color: 'red', backgroundColor: 'yellow'}}>serap.com</h1>
+            <label htmlFor="search" tabIndex="2" onClick={() => alert('merhaba')}>Arama</label>
+            <input type="text" id="search" tabIndex="1" />
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default App;
