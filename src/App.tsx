6605fb844dc872b4544842980ce7bf399bbeeed5
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2024-04-02 14:40:44 +0300

    vite env calisti

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
+
 function App() {
+
+    console.log(process.env.REACT_APP_API_URL)
 
   return (
       <div>
+
           <h1>Hello World</h1>
-          <h1>Hello World</h1>
+
+          <p>{process.env.REACT_APP_API_URL}</p>
+
+
       </div>
   )
 }
